a0bad7933d986e62640f604b5f20a2e42e12c494
duhnlic <bpstew22@gmail.com>
2021-03-18 20:59:39 -0500

    My temp now turns blue when below 40 and red when above 90

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 const input = document.querySelector("#user-input");
 const submit = document.querySelector("#submit")
 const weatherContainer = document.querySelector("#weather-container")
-    //FUNCTION TO GET THE INFO FROM THE API
 
 const addTempDom = (data) => {
     console.log(data);
     const weatherDiv = `<div>${data.name}</div>
     </div>The Current Weather: ${data.weather[0].description}</div> 
-    <div>It is currently: ${data.main.temp}</div> 
+    <div>It is currently:</div>
+    <div id="temp"> ${data.main.temp}</div> 
     <div>With a High of ${data.main.temp_max} and a low of ${data.main.temp_min}
-        </div>`
+    </div>`
     weatherContainer.innerHTML = weatherDiv;
 }
 
+//FUNCTION TO GET THE INFO FROM THE API
 const weatherTime = async() => {
 
     const apiKey = ",us&units=imperial&appid=39da52614d4a14d697506d6ed598e957";
@@ -24,8 +25,17 @@ const weatherTime = async() => {
         const data = await response.json();
         // addWeatherDom(data);
         addTempDom(data);
+        //create an if else statement to determine the color of the temperature reading based on the temperature data given from the API
+        if (data.main.temp < 40) {
+            temp.style.color = "blue";
+        } else if (data.main.temp > 90) {
+            temp.style.color = "red";
+        }
     } catch (err) {
         console.log(err);
     }
 }
+
+
+
 submit.addEventListener("click", weatherTime);
